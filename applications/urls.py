6a57ae5e0abe5fc52246320e8695ac163ebfffc4
Swapnil Khambayat <swapniljkhambayat27@gmail.com>
2026-02-03 17:36:01 +0530

    Refactor templates to use base layout and fix duplicate page header

--- applications/urls.py
+++ applications/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("my-applications/", my_applications, name="my_applications"),
-    # path("view-applications/", view_applications, name="view_applications"),
     path(
             "application/<int:app_id>/status/<str:status>/",
             update_application_status,
